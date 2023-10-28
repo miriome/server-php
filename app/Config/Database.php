6c2daa8e9d5fb4c5f0ea2b32f36e0f07dbfdaec4
Winson Heng <winsonheng@hotmail.com>
2023-10-28 15:50:47 +0800

    fix: emojigst

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -24,32 +24,12 @@ class Database extends Config
     /**
      * The default database connection.
      */
-    public array $default = [
-        'DSN'      => '',
-        'hostname' => '184.168.98.206',
-        'username' => 'miromie_root',
-        'password' => 'KIaR*~E,L3D.',
-        'database' => 'miromie',
-        'DBDriver' => 'MySQLi',
-        'DBPrefix' => '',
-        'pConnect' => false,
-        'DBDebug'  => true,
-        'charset'  => 'utf8',
-        'DBCollat' => 'utf8_general_ci',
-        'swapPre'  => '',
-        'encrypt'  => false,
-        'compress' => false,
-        'strictOn' => false,
-        'failover' => [],
-        'port'     => 3306,
-    ];
-
     // public array $default = [
     //     'DSN'      => '',
-    //     'hostname' => 'localhost',
-    //     'username' => 'root',
-    //     'password' => 'winson',
-    //     'database' => 'miromie-local',
+    //     'hostname' => '184.168.98.206',
+    //     'username' => 'miromie_root',
+    //     'password' => 'KIaR*~E,L3D.',
+    //     'database' => 'miromie',
     //     'DBDriver' => 'MySQLi',
     //     'DBPrefix' => '',
     //     'pConnect' => false,
@@ -63,50 +43,71 @@ class Database extends Config
     //     'failover' => [],
     //     'port'     => 3306,
     // ];
-    
+
+    public array $default = [
+        'DSN' => '',
+        'hostname' => 'localhost',
+        'username' => 'root',
+        'password' => 'winson',
+        'database' => 'miromie-local',
+        'DBDriver' => 'MySQLi',
+        'DBPrefix' => '',
+        'pConnect' => false,
+        'DBDebug' => true,
+        'charset' => 'utf8mb4',
+        'DBCollat' => 'utf8mb4_unicode_ci',
+        'swapPre' => '',
+        'encrypt' => false,
+        'compress' => false,
+        'strictOn' => false,
+        'failover' => [],
+        'port' => 3306,
+    ];
+
     // Doesnt work lmao
-    // public array $local = [
-    //     'DSN'      => '',
-    //     'hostname' => 'localhost',
-    //     'username' => 'root',
-    //     'password' => 'winson',
-    //     'database' => 'miromie-local',
-    //     'DBDriver' => 'MySQLi',
-    //     'DBPrefix' => '',   
-    //     'pConnect' => false,
-    //     'DBDebug'  => true,
-    //     'charset'  => 'utf8',
-    //     'DBCollat' => 'utf8_general_ci',
-    //     'swapPre'  => '',
-    //     'encrypt'  => false,
-    //     'compress' => false,
-    //     'strictOn' => false,
-    //     'failover' => [],
-    //     'port'     => 3306,
-    // ];
+    public array $local = [
+        'DSN' => '',
+        'hostname' => 'localhost',
+        'username' => 'root',
+        'password' => 'winson',
+        'database' => 'miromie-local',
+        'DBDriver' => 'MySQLi',
+        'DBPrefix' => '',
+        'pConnect' => false,
+        'DBDebug' => true,
+        'charset' => 'utf8',
+        'DBCollat' => 'utf8_general_ci',
+        'swapPre' => '',
+        'encrypt' => false,
+        'compress' => false,
+        'strictOn' => false,
+        'failover' => [],
+        'port' => 3306,
+    ];
 
     /**
      * This database connection is used when
      * running PHPUnit database tests.
      */
     public array $tests = [
-        'DSN'         => '',
-        'hostname'    => '127.0.0.1',
-        'username'    => '',
-        'password'    => '',
-        'database'    => ':memory:',
-        'DBDriver'    => 'SQLite3',
-        'DBPrefix'    => 'db_',  // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI DEVS
-        'pConnect'    => false,
-        'DBDebug'     => true,
-        'charset'     => 'utf8',
-        'DBCollat'    => 'utf8_general_ci',
-        'swapPre'     => '',
-        'encrypt'     => false,
-        'compress'    => false,
-        'strictOn'    => false,
-        'failover'    => [],
-        'port'        => 3306,
+        'DSN' => '',
+        'hostname' => '127.0.0.1',
+        'username' => '',
+        'password' => '',
+        'database' => ':memory:',
+        'DBDriver' => 'SQLite3',
+        'DBPrefix' => 'db_',
+        // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI DEVS
+        'pConnect' => false,
+        'DBDebug' => true,
+        'charset' => 'utf8',
+        'DBCollat' => 'utf8_general_ci',
+        'swapPre' => '',
+        'encrypt' => false,
+        'compress' => false,
+        'strictOn' => false,
+        'failover' => [],
+        'port' => 3306,
         'foreignKeys' => true,
         'busyTimeout' => 1000,
     ];
